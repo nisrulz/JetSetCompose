cf9cfa5ac0e179b727b8e59e536ac425ff85c9d5
Nishant Srivastava <nisrulz@gmail.com>
2021-05-12 16:10:01 +0200

    setup completed widgets

---
+++ AndroidApp/app/src/main/java/com/nisrulz/androidapp/custom/BuyMeACofffeeWidget.kt
@@ -0,0 +1,96 @@
+package com.nisrulz.androidapp.custom
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.nisrulz.androidapp.ui.theme.AndroidAppTheme
+
+@Composable
+fun CoffeeMultiplierBox(onValueChange: (Int) -> Unit) {
+    Card(
+        shape = RoundedCornerShape(3.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(MaterialTheme.colors.secondary)
+                .border(1.dp, MaterialTheme.colors.primaryVariant)
+                .padding(10.dp),
+        ) {
+            Text(text = "☕️ x ", textAlign = TextAlign.Center)
+            MultiplierValue(value = 1) {
+                onValueChange(it)
+            }
+            MultiplierValue(value = 3) {
+                onValueChange(it)
+            }
+            MultiplierValue(value = 5) {
+                onValueChange(it)
+            }
+        }
+    }
+}
+
+@Composable
+fun BuyMeACoffeeWidget(name: String) {
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        elevation = 8.dp
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(25.dp)
+        ) {
+
+            val multiplier = remember { mutableStateOf(1) }
+            val nameOfUser = remember { mutableStateOf("") }
+            val message = remember { mutableStateOf("") }
+            val payPalUsername = remember { mutableStateOf("") }
+
+            HeaderText(name = name)
+            Spacer(modifier = Modifier.height(15.dp))
+            CoffeeMultiplierBox {
+                multiplier.value = it
+            }
+
+            InputBox(value = nameOfUser.value, "Name or @your_twitter (optional)") {
+                nameOfUser.value = it
+            }
+            Spacer(modifier = Modifier.height(10.dp))
+            InputBox(
+                value = message.value,
+                placeholder = "Say something nice.. (optional)",
+                modifier = Modifier.height(110.dp)
+            ) {
+                message.value = it
+            }
+            Spacer(modifier = Modifier.height(30.dp))
+
+            val finalAmount = 5 * multiplier.value
+            ActionButton(finalAmount)
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+    name = "Buy Me A Coffee Widget",
+)
+@Composable
+fun DefaultPreview() {
+    AndroidAppTheme() {
+        BuyMeACoffeeWidget("GDG London Team")
+    }
+}
---
+++ AndroidApp/app/src/main/java/com/nisrulz/androidapp/custom/CustomWidgets.kt
@@ -0,0 +1,106 @@
+package com.nisrulz.androidapp.custom
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun HeaderText(name: String) {
+    Text(
+        buildAnnotatedString {
+            append("Buy ")
+            withStyle(SpanStyle(color = Color.Gray)) {
+                append(name)
+            }
+            append(" a coffee")
+        },
+        fontWeight = FontWeight.Bold,
+        fontSize = 25.sp
+    )
+}
+
+@Composable
+fun MultiplierValue(value: Int, onClickOfItem: (Int) -> Unit) {
+    Box(
+        modifier = Modifier
+            .width(20.dp)
+            .height(20.dp)
+            .padding(2.dp)
+            .offset(y = 3.dp)
+            .background(MaterialTheme.colors.primary, CircleShape)
+            .border(1.dp, MaterialTheme.colors.primaryVariant, CircleShape),
+        contentAlignment = Alignment.Center
+    ) {
+        ClickableText(buildAnnotatedString {
+            withStyle(
+                SpanStyle(
+                    fontSize = 10.sp,
+                    color = Color.White,
+                )
+            ) { append(value.toString()) }
+        }) {
+            onClickOfItem(value)
+        }
+
+    }
+}
+
+
+@Composable
+fun InputBox(
+    value: String,
+    placeholder: String,
+    modifier: Modifier = Modifier,
+    onValueChange: (String) -> Unit
+) {
+    OutlinedTextField(
+        modifier = modifier.fillMaxWidth(),
+        value = value,
+        onValueChange = { onValueChange(it) },
+        placeholder = {
+            Text(text = placeholder)
+        },
+        keyboardOptions = KeyboardOptions(
+            capitalization = KeyboardCapitalization.None,
+            autoCorrect = true,
+            keyboardType = KeyboardType.Text,
+        )
+    )
+}
+
+@Composable
+fun ActionButton(finalAmount: Int) {
+    val context = LocalContext.current
+    val intent = Utils.getPayPalLink(finalAmount)
+
+    Button(
+        modifier = Modifier
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(18.dp),
+        onClick = { context.startActivity(intent) }
+    ) {
+        Text(text = "Support $$finalAmount")
+    }
+}
