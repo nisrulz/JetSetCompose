132f76f267b3e0d6b6e4a3ac17f43442d1a200c6
Nishant Srivastava <nisrulz@gmail.com>
2021-11-10 01:07:47 +0100

    cleanup the main class for Desktop app

--- DesktopApp/src/main/kotlin/main.kt
+++ DesktopApp/src/main/kotlin/main.kt
@@ -1,18 +1,11 @@
-import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.application
-import androidx.compose.ui.window.rememberWindowState
+import androidx.compose.ui.window.singleWindowApplication
 import custom.BuyMeACoffeeWidget
 import custom.DesktopTheme
 
-fun main() = application {
-    val windowState = rememberWindowState()
-    Window(
-        onCloseRequest = ::exitApplication,
-        state = windowState,
-        title = "JetSetCompose-Desktop"
-    ) {
-        DesktopTheme {
-            BuyMeACoffeeWidget("GDG UK")
-        }
+fun main() = singleWindowApplication(
+    title = "JetSetCompose-Desktop"
+) {
+    DesktopTheme {
+        BuyMeACoffeeWidget("GDG UK")
     }
 }
