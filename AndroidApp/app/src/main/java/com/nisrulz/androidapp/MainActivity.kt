8690a040c97444f5445d886bd10a012d42b3ab40
Nishant Srivastava <nisrulz@gmail.com>
2021-11-10 01:13:50 +0100

    update android app

--- AndroidApp/app/src/main/java/com/nisrulz/androidapp/MainActivity.kt
+++ AndroidApp/app/src/main/java/com/nisrulz/androidapp/MainActivity.kt
@@ -3,9 +3,6 @@ package com.nisrulz.androidapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.nisrulz.androidapp.custom.BuyMeACoffeeWidget
